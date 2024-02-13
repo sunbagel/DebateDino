9675c8ccb2963afa0098f67b684de760e378748a
sunbagel <43621165+sunbagel@users.noreply.github.com>
2024-02-12 21:51:20 -0500

    neaten navbar styles

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import '../styles/NavBar.css'
 
 const NavBar = () => {
 
@@ -20,7 +21,7 @@ const NavBar = () => {
               <img src="../../walterworth.png" alt="User profile" className="object-cover h-full w-full" />
             </div>
           </button>
-          <a href="#" className="p-2 lg:px-4 md:mx-2 text-gray-800 text-center border border-transparent rounded hover:bg-gray-100 hover:text-gray-700 transition-colors duration-300">
+          <a href="#" className="navbar-link">
             Profile
             
             </a>
@@ -34,15 +35,15 @@ const NavBar = () => {
 
             {/* links */}
             <div className="flex flex-col md:flex-row md:ml-6">
-                <a href="#" className="p-2 lg:px-4 md:mx-2 text-gray-800 text-center border border-transparent rounded hover:bg-gray-100 hover:text-gray-700 transition-colors duration-300">Home</a>
-                <a href="#" className="p-2 lg:px-4 md:mx-2 text-gray-800 text-center border border-transparent rounded hover:bg-gray-100 hover:text-gray-700 transition-colors duration-300">About</a>
-                <a href="#" className="p-2 lg:px-4 md:mx-2 text-gray-800 text-center border border-transparent rounded hover:bg-gray-100 hover:text-gray-700 transition-colors duration-300">Tournaments</a>
+                <a href="#" className="navbar-link">Home</a>
+                <a href="#" className="navbar-link">About</a>
+                <a href="#" className="navbar-link">Tournaments</a>
             </div>
 
             {/* search */}
             <div className="md:flex md:justify-end mt-2 md:mt-0 md:w-1/2 lg:w-1/4">
                 <div className="relative">
-                <input type="text" className="w-full py-2 pl-10 pr-4 border rounded-md focus:border-blue-500 focus:outline-none focus:shadow-outline" placeholder="Search" />
+                <input type="text" className="search-bar" placeholder="Search" />
                 <div className="absolute top-0 left-0 inline-flex items-center p-2">
                     {/* <SearchIcon className="h-6 w-6 text-gray-500" /> */}
                 </div>
