bef9a254f4bb6a1acd59856c3b9bf61a97439682
sunbagel <43621165+sunbagel@users.noreply.github.com>
2024-02-23 22:23:43 -0500

    fetch user data in UserProfile page

--- frontend/src/pages/UserProfile.tsx
+++ frontend/src/pages/UserProfile.tsx
@@ -1,6 +1,36 @@
+import axios from "@/lib/axios";
+import { User } from "@/types/users"
+import { useEffect, useState } from "react";
 
 
 const UserProfile = () => {
+
+  const [ userInfo, setUserInfo ] = useState<User>();
+  const userIDPlaceholder = "65d69c469d47c04d60421fdb";
+
+  const tempUser = {
+    "name": "alex2",
+    "password": "abc123",
+    "email": "alex@gmail.com",
+    "institution": "Queens University",
+    "agreement": "i agree",
+    "participating": [],
+    "judging": [],
+    "hosting": []
+  }
+
+  useEffect(()=>{
+    // api should not return password. may want to return id?
+    axios.get(`user/${userIDPlaceholder}`)
+    .then(res => {
+      console.log(res.data);
+      const userRes : User = res.data;
+      setUserInfo(userRes);
+    })
+    .catch(err => console.log(err))
+      
+  }, [setUserInfo])
+
   return (
     <div className="max-w-6xl mx-auto p-4">
       <h1 className="text-5xl font-bold">Profile Page</h1>
