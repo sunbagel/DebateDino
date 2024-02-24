4eaf4cb3f13efd4c9049734e9880f7a425391554
Kevin Tan <tankevin793@gmail.com>
2024-02-23 20:42:43 -0500

    updated tournament creation and added api call on tournament page

--- frontend/src/pages/Tournaments/Tournaments.tsx
+++ frontend/src/pages/Tournaments/Tournaments.tsx
@@ -1,8 +1,36 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from 'axios'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/shadcn-components/ui/card";
+
+
+type Tournament = {
+    host: string;
+	name: string;
+	description: string;
+	location: string;
+	date: string;
+	image: string;
+	debaters: string[];
+	judges: string[];
+	refundPolicy: string;
+}
 
 const Tournaments = () => {
 
     const navigate = useNavigate();
+    const [tournaments, setTournaments] = useState<Array<Tournament>>([]);
+
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_API_SERVER}/api/tournaments_test`)
+        .then(res => {
+            const tournamentRes: Tournament[] = res.data;
+            return setTournaments(tournamentRes);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [setTournaments])
 
     return (
         <div className="container mx-auto flex min-h-screen flex-col">
@@ -13,6 +41,27 @@ const Tournaments = () => {
                 <div className="flex">
                     <button onClick={() => navigate('create')} className="px-6 py-2 bg-black text-white rounded">Create Tournament</button>
                 </div>
+            </div>
+            <div className="pt-10 flex gap-10 flex-wrap">
+                {tournaments.map((tournament) => {
+                    return (
+                        <Card>
+                            <CardHeader className="pt-5 flex justify-center items-center">
+                                <img src="../../walterworth.png" alt="User profile" className="max-w-48 rounded-lg" />
+                            </CardHeader>
+                            <CardContent>
+                                <CardTitle>{tournament.name}</CardTitle>
+                                <CardDescription>{tournament.description}</CardDescription>
+                            </CardContent>
+                            <CardContent>
+                                <CardDescription>{tournament.location}</CardDescription>
+                            </CardContent>
+                            <CardFooter>
+                                <p>Insert host name</p>
+                            </CardFooter>
+                        </Card>
+                    )
+                })}
             </div>
         </div>
 
