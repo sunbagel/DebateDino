5f250f86c5ba218795433f8631a121958234032c
sunbagel <43621165+sunbagel@users.noreply.github.com>
2024-03-24 21:32:33 -0400

    return auth client

--- backend/db/fbAuth.go
+++ backend/db/fbAuth.go
@@ -2,20 +2,29 @@ package db
 
 import (
 	"context"
-	"fmt"
+	"log"
 
-	firebase "firebase.google.com/go"
+	firebase "firebase.google.com/go/v4"
+	"firebase.google.com/go/v4/auth"
+
 	"google.golang.org/api/option"
 )
 
-func InitFirebaseAuth() *firebase {
+func InitFirebaseAuth() *auth.Client {
 
 	ctx := context.Background()
 
 	opt := option.WithCredentialsFile("./serviceAccountKey.json")
 	app, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
-		return nil, fmt.Errorf("error initializing app: %v", err)
+		log.Fatalf("error initializing app: %v\n", err)
 	}
+
+	authClient, err := app.Auth(ctx)
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v\n", err)
+	}
+
+	return authClient
 
 }
