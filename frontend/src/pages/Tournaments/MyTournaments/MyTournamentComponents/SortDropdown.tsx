e7f7e6d4211d3da9075c34d21c1c6be6c771d277
sunbagel <43621165+sunbagel@users.noreply.github.com>
2024-12-24 20:51:58 -0500

    Improve type checking for SortDropdown

--- frontend/src/pages/Tournaments/MyTournaments/MyTournamentComponents/SortDropdown.tsx
+++ frontend/src/pages/Tournaments/MyTournaments/MyTournamentComponents/SortDropdown.tsx
@@ -13,11 +13,29 @@ import { useState } from "react"
 
 // props for the options
 interface SortDropdownProps {
-    position: SortOption;
-    setPosition: (value: SortOption) => void;
+    option: SortOption;
+    setOption: (value: SortOption) => void;
 }
 
-const SortDropdown : React.FC<SortDropdownProps> = ({position, setPosition}) => {
+const SortDropdown : React.FC<SortDropdownProps> = ({option, setOption}) => {
+
+    const DEFAULT_SORT_OPTION: SortOption = "name";
+
+    // type checker
+    const isSortOption = (value: string): value is SortOption => {
+        return ["name", "host", "date", "duration", "dinos"].includes(value);
+    };
+
+    const selectOption = (value: string) => {
+        if(isSortOption(value)){
+            // type safe
+            setOption(value);
+        } else {
+            console.warn("Error selecting a sort option received: ${value}. Reverting to default sorting.");
+            setOption(DEFAULT_SORT_OPTION);
+        }
+    }
+    
 
     return (
         <DropdownMenu>
@@ -25,7 +43,7 @@ const SortDropdown : React.FC<SortDropdownProps> = ({position, setPosition}) => 
                 <Button variant="outline">Sort By</Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56">
-                <DropdownMenuRadioGroup value={position} onValueChange={(value) => setPosition(value as SortOption)}>
+                <DropdownMenuRadioGroup value={option} onValueChange={selectOption}>
                     <DropdownMenuRadioItem value="name">Name</DropdownMenuRadioItem>
                     <DropdownMenuRadioItem value="host">Host</DropdownMenuRadioItem>
                     <DropdownMenuRadioItem value="date">Date</DropdownMenuRadioItem>
